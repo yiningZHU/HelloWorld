a63794a84e36eb26c85af0aa079872a539370ae8
Yining <1846364433@qq.com>
2016-09-20 11:20:53 +0800

    Revert "不是瞎写的，在尝试输出星号，基本明白了。就"
    This reverts commit 2c1efc74728730bb01a9c5cf6f61c81b6bed968c.

--- pyramid/p1.ts
+++ pyramid/p1.ts
@@ -1,31 +1,8 @@
 function print (a: number)
 {
-    var space: string = " ";
-    var star: string = "*";
-    var forspa: string = "";
-    var forsta: string = "";
-    var test: string = "";
-
     for(var i: number = 1;i<=a;i++)
     {
-        //for(var j: number = 1;j<=a*2-1;j++)
-        //{
-           for(var k: number = 1;k<=a-1;k++)
-           {
-               forspa= forspa + space;
-           }
-           for(var x: number = 0;x<i*2-1;x++)
-           {
-               forsta = forsta + star;
-           }
-           //for(var y: number = 1;y<=a-1;y++)
-           //{
-           test = forspa+forsta;
-           console.log(test);
-        //}
-           //test = space + test;
-           
+        console.log("*");
     }
-    //}
 }
-print(2);
+print(3);
