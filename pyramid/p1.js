d89f9e58bf1646cd153fc44e744db4e4c33ace6f
Yining <1846364433@qq.com>
2016-09-20 14:15:29 +0800

    还是暂时有点错误，我也不太清楚哪里错了。等会儿询问一下已经完成的同学。

--- pyramid/p1.js
+++ pyramid/p1.js
@@ -1,6 +1,18 @@
 function print(a) {
+    var space = "  ";
+    var star = "*";
+    var test = "";
     for (var i = 1; i <= a; i++) {
-        console.log("*");
+        //for(var j: number = 1;j<=a*2-1;j++)
+        //{
+        for (var k = 1; k <= a - i; k++) {
+            test = test + space;
+        }
+        for (var x = 1; x <= i * 2 - 1; x++) {
+            test = test + star;
+        }
+        console.log(test);
     }
+    //}
 }
-print(3);
+print(1);
--- pyramid/p1.ts
+++ pyramid/p1.ts
@@ -1,8 +1,24 @@
 function print (a: number)
 {
+    var space: string = " ";
+    var star: string = "*";
+    var test: string = "";
+
     for(var i: number = 1;i<=a;i++)
     {
-        console.log("*");
+        //for(var j: number = 1;j<=a*2-1;j++)
+        //{
+           for(var k: number = 1;k<=a-i;k++)
+           {
+               test = test + space;
+           }
+           for(var x: number = 1;x<=i*2-1;x++)
+           {
+               test = test + star;
+           }
+           
+           console.log(test);
     }
+    //}
 }
-print(3);
+print(2);
