c453150dcfb409040d1c686bc35343f7f79a580a
Yining <1846364433@qq.com>
2016-09-20 09:53:51 +0800

    好不容易配置好了，随便试一下代码。

---
+++ pyramid/p1.js
@@ -0,0 +1,6 @@
+function print(a) {
+    for (var i = 1; i <= a; i++) {
+        console.log("*");
+    }
+}
+print(3);
---
+++ pyramid/p1.ts
@@ -0,0 +1,8 @@
+function print (a: number)
+{
+    for(var i: number = 1;i<=a;i++)
+    {
+        console.log("*");
+    }
+}
+print(3);
