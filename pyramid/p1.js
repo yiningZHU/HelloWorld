2c1efc74728730bb01a9c5cf6f61c81b6bed968c
Yining <1846364433@qq.com>
2016-09-20 11:20:05 +0800

    不是瞎写的，在尝试输出星号，基本明白了。就

--- pyramid/p1.js
+++ pyramid/p1.js
@@ -1,6 +1,23 @@
 function print(a) {
+    var space = " ";
+    var star = "*";
+    var forspa = "";
+    var forsta = "";
+    var test = "";
     for (var i = 1; i <= a; i++) {
-        console.log("*");
+        //for(var j: number = 1;j<=a*2-1;j++)
+        //{
+        for (var k = 1; k <= a - 1; k++) {
+            forspa = forspa + space;
+        }
+        for (var x = 0; x < i * 2 - 1; x++) {
+            forsta = forsta + star;
+        }
+        //for(var y: number = 1;y<=a-1;y++)
+        //{
+        test = forspa + forsta;
+        console.log(test);
     }
+    //}
 }
-print(3);
+print(2);
